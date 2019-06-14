27ae2d53b15412d8e1ca13ffa126a67b1190dd83
Shuo <anshuobuaa@gmail.com>
2019-06-14 11:36:14 +0100

    second version, add functions: random sleep, fake-agent change

--- mzitu_crawler.py
+++ mzitu_crawler.py
@@ -2,29 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 import os
+import random
+from fake_useragent import UserAgent
 
 IMAGE_PATH = os.getcwd() + '/images/'
 
 
-def header(referer):
+def header(fake_ua, referer):
     '''
-    You should always update the referer, even when you are doing test
+    Using the fake_useragent to randomly choose your useragent to avoid the anti-spider tech.
+    You should always update the referer, even when you are doing test.
     '''
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/59.0.3071.115 Safari/537.36',
+        'User-Agent': fake_ua,
         'Referer': '{}'.format(referer),
     }
     return headers
 
 
-def request_page(pageNum, referer):
+def request_page(pageNum, fake_ua, referer):
     '''
     Get the certain page information
     '''
     baseUrl = 'https://www.mzitu.com/page/{}/'.format(pageNum)
 
-    r = requests.get(baseUrl, headers=header(referer))
+    r = requests.get(baseUrl, headers=header(fake_ua, referer))
     print(r)  # check the response
     return r
 
@@ -40,10 +42,27 @@ def extract_links(response_info):
     return link_list
 
 
+def random_sleep_time():
+    '''
+    random generate the time of sleeping when doing the scraping work
+    '''
+    possibility_like = random.random()
+    if possibility_like < 0.2:
+        sleep_time = random.randint(10, 20)
+    else:
+        sleep_time = random.randint(1, 10)
+    return sleep_time
+
+
 def get_images(blog_url, referer):
-    print(requests.get((blog_url), headers=header(referer)))
-    soup = BeautifulSoup(requests.get(
-        blog_url, headers=header(referer)).text, features="html.parser")
+    # ---- Every time when choosing girl, randomly choose one useragent ----
+    fake_ua = UserAgent()
+    user_agent = fake_ua.random
+
+    blog_first_r = requests.get(
+        (blog_url), headers=header(user_agent, referer))
+    print(blog_first_r)
+    soup = BeautifulSoup(blog_first_r.text, features="html.parser")
 
     # ---- get the number of images ----
     pic_num = soup.find('div', class_='pagenavi').find_all('a')[4].get_text()
@@ -62,24 +81,38 @@ def get_images(blog_url, referer):
     print('downloading' + dir_name + '...')
 
     # ---- downloading the images ----
-    image_referer = referer
-    for pic_index in range(1, int(pic_num) + 1):
+    # download the first image
+    image_referer = blog_url
+    pic_src = soup.find('div', 'main-image').find('img')['src']
+    pic_name = pic_src.split('/')[-1]
+
+    sleep(random_sleep_time())
+    f = open(pic_name, 'wb')
+    print("accessing {}".format(pic_src))
+    f.write(requests.get(pic_src, headers=header(
+        user_agent, image_referer)).content)
+    f.close()
+    print("finish the first image : {}".format(pic_name))
+
+    for pic_index in range(2, int(pic_num) + 1):
         # setting the time gap is important, or it will meet the Max retries exceeded with url error
-        sleep(1)
+        sleep(random_sleep_time())
         pic_link = blog_url + '/' + str(pic_index)
-        pic_cur_page = requests.get(pic_link, headers=header(image_referer))
+        pic_cur_page = requests.get(
+            pic_link, headers=header(user_agent, image_referer))
         soup = BeautifulSoup(pic_cur_page.text, features="html.parser")
         pic_src = soup.find('div', 'main-image').find('img')['src']
         pic_name = pic_src.split('/')[-1]
 
         f = open(pic_name, 'wb')
         print("accessing {}".format(pic_src))
-        f.write(requests.get(pic_src, headers=header(image_referer)).content)
+        f.write(requests.get(pic_src, headers=header(
+            user_agent, image_referer)).content)
         f.close()
         image_referer = pic_link  # update the referer
         print("finish {} image : {}".format(pic_index, pic_name))
 
-    os.chdir(IMAGE_PATH)  # finish downloading, and get back
+    os.chdir(IMAGE_PATH)  # finish downloading, and get back the dir
 
 
 if __name__ == "__main__":
@@ -89,9 +122,12 @@ if __name__ == "__main__":
     header_referer = "https://www.mzitu.com/"
 
     # by setting the range, can scrape the images in the first n pages.
-    for i in range(3):
+    for i in range(3, 4):
         print("accessing the {} page...".format(i))
-        r = request_page(i, referer=header_referer)
+        fake_ua = UserAgent()
+        user_agent = fake_ua.random
+
+        r = request_page(i, user_agent, header_referer)
         current_page = 'https://www.mzitu.com/page/{}/'.format(i)
         header_referer = current_page
         # ---- extract all the links from one page----
